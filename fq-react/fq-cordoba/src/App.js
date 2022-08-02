392eb6cde76e2c031012947744de6e98af533198
maximomazzuchin <53264226+maximomazzuchin@users.noreply.github.com>
2022-08-02 10:21:43 -0300

    Add files via upload

--- fq-react/fq-cordoba/src/App.js
+++ fq-react/fq-cordoba/src/App.js
@@ -1,27 +1,19 @@
  import React, {useState, useEffect} from 'react';
  import './App.css';
  import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+ import { Navbar } from "../src/components/navbar/Navbar";
  import Login from "./paginas/Login";
  import Home from "./paginas/Home";
- 
+
 
 function App() {
-
-  /*const[scrollHeight, setScrollHeight] = useState(0);
-
-  const handleScroll = () => {
-    const position = window.pageXOffset;
-    setScrollHeight = (position); 
-  };
-
-  useEffect (() => {
-    window.addEventListener("scroll", handleScroll);
-  }, [scrollHeight])*/
-
 
   
   return (
       <Router>
+         <div className="Navbar">
+            <Navbar/>
+          </div>  
         <Routes>
           <Route path="/home" element= {<Home />} />
           <Route path="/login" element = {<Login />} />
