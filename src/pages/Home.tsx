f596f3dd4480c49a66031840e4b0d118f2b55b35
maximomazzuchin <53264226+maximomazzuchin@users.noreply.github.com>
2022-09-28 13:11:11 -0300

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,14 +2,9 @@ import React, { useEffect } from 'react'
 import videoBG from '../media/new-bg-2.mp4'
 import './Home.css'
 export function Home() {
-  const url = "http://10.20.19.58:8000/api/products"
+  const url = "http://10.20.19.58:8000/api/products/?format=json"
   const fetchApi = async () =>{
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "access-control-allow-origin" : "*",
-        "Content-type": "application/json; charset=UTF-8"
-      }})
+    const response = await fetch(url)
     console.log(response.status)
   }
 
